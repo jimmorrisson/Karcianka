9c666a66c198eb3235ccc8bcbc15f5a1db02dfec
Pc-Komputer\Pc <pyszczakjakub@gmail.com>
2018-03-29 11:57:18 +0200

    Networking - basic turn based system

---
+++ Karcianka/Assets/Scripts/Networking/GameManager.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Com.Polygon.CardGame
+{
+	public class GameManager : Photon.PunBehaviour, IPunTurnManagerCallbacks
+	{
+		private PunTurnManager turnManager;
+
+		/// <summary>
+		/// The duration of the turn in seconds.
+		/// </summary>
+		public float TurnDuration;
+
+		public Text TimeText;
+		public Text PlayerText;
+		public Text RemotePlayerText;
+
+		private void Start()
+		{
+			this.turnManager = this.gameObject.AddComponent<PunTurnManager>();
+			this.turnManager.TurnManagerListener = this;
+			this.turnManager.TurnDuration = 5f;
+
+			PlayerText.text = PhotonNetwork.player.NickName;
+		}
+
+		private void Update()
+		{
+			this.TimeText.text = "Time: " + string.Format("{0:0.00}", this.turnManager.RemainingSecondsInTurn);
+			UpdateTimeText();
+		}
+
+		public void StartTurn()
+		{
+			if (PhotonNetwork.isMasterClient)
+			{
+				Debug.Log("Begin Turn");
+				this.turnManager.BeginTurn();
+			}
+		}
+
+		private void UpdateTimeText()
+		{
+			PhotonPlayer remote = PhotonNetwork.player.GetNext();
+			PhotonPlayer local = PhotonNetwork.player;
+
+			if(remote != null)
+			{
+				RemotePlayerText.text = remote.NickName;
+			}
+			else
+			{
+				RemotePlayerText.text = "waiting for another player...";
+			}
+		}
+
+        //private void LoadArena()
+        //{
+        //    if (!PhotonNetwork.isMasterClient)
+        //    {
+        //        Debug.LogError("PhotonNetwork : Trying to Load a level but we are not the master Client");
+        //    }
+        //    Debug.Log("Load main scene");
+        //    PhotonNetwork.LoadLevel("Main");
+        //}
+        public override void OnLeftRoom()
+		{
+			SceneManager.LoadScene(0);
+		}
+
+		public void LeaveRoom()
+		{
+			PhotonNetwork.LeaveRoom();
+		}
+
+		#region Photon Messages
+
+
+		public override void OnPhotonPlayerConnected(PhotonPlayer other)
+		{
+			Debug.Log("Other player arrived " + other.NickName); // not seen if you're the player connecting
+
+			if (PhotonNetwork.room.PlayerCount== 2)
+			{
+				Debug.Log("Starting turn...");
+				this.StartTurn();
+			}
+		}
+
+
+		public override void OnPhotonPlayerDisconnected(PhotonPlayer other)
+		{
+			Debug.Log("OnPhotonPlayerDisconnected() " + other.NickName); // seen when other disconnects
+
+
+			if (PhotonNetwork.isMasterClient)
+			{
+				Debug.Log("OnPhotonPlayerDisonnected isMasterClient " + PhotonNetwork.isMasterClient); // called before OnPhotonPlayerDisconnected
+			}
+		}
+
+		public override void OnJoinedRoom()
+		{
+			if (PhotonNetwork.room.PlayerCount == 2)
+			{
+				if (this.turnManager.Turn == 0)
+				{
+					Debug.Log("There are 2 players. Starting the game");
+					// when the room has two players, start the first turn (later on, joining players won't trigger a turn)
+					this.StartTurn();
+				}
+			}
+			else
+			{
+				Debug.Log("Waiting for another player");
+			}
+		}
+
+		public void OnTurnBegins(int turn)
+		{
+			Debug.Log("On turn " + turn);
+		}
+
+		public void OnTurnCompleted(int turn)
+		{
+			Debug.Log("OnTurnCompleted! " + turn);
+			this.StartTurn();
+		}
+
+		public void OnPlayerMove(PhotonPlayer player, int turn, object move)
+		{
+
+		}
+
+		public void OnPlayerFinished(PhotonPlayer player, int turn, object move)
+		{
+
+		}
+
+		public void OnTurnTimeEnds(int turn)
+		{
+			OnTurnCompleted(-1);
+		}
+
+
+		#endregion
+	}
+}
---
+++ Karcianka/Assets/Scripts/Networking/Launcher.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Com.Polygon.CardGame
+{
+    public class Launcher : Photon.PunBehaviour
+    {
+        /// <summary>
+        /// This client's version number
+        /// </summary>
+        private string _gameVersion = "2";
+        /// <summary>
+        /// Keep track of the current process. Since connection is asynchronous and is based on several callbacks from Photon, 
+        /// we need to keep track of this to properly adjust the behavior when we receive call back by Photon.
+        /// Typically this is used for the OnConnectedToMaster() callback.
+        /// </summary>
+        private bool isConnecting;
+        [SerializeField] private Button submitBtn;
+
+        /// <summary>
+        /// The PUN loglevel
+        /// </summary>
+        public PhotonLogLevel LogLevel = PhotonLogLevel.Informational;
+        public byte MaxPlayersPerRoom = 2;
+        [Tooltip("The Ui Panel to let the user enter name, connect and play")]
+        public GameObject controlPanel;
+        [Tooltip("The UI Label to inform the user that the connection is in progress")]
+        public GameObject progressLabel;
+        public Text nickName;
+
+        private void Awake()
+        {
+            PhotonNetwork.logLevel = LogLevel;
+            // we don't join to lobby. There is no need to join a lobby to get the list of rooms
+            PhotonNetwork.autoJoinLobby = false;
+
+            // this makes sure we can use PhotonNetwork.LoadLevel() on the master client and all clients in the same room sync their level automatically
+            PhotonNetwork.automaticallySyncScene = true;
+        }
+
+        private void Start()
+        {
+            progressLabel.SetActive(false);
+            controlPanel.SetActive(true);
+            //Connect();
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Return))
+                submitBtn.onClick.Invoke();
+        }
+
+        /// <summary>
+        /// Start the connection process. 
+        /// - If already connected, we attempt joining a random room
+        /// - if not yet connected, Connect this application instance to Photon Cloud Network
+        /// </summary>
+        public void Connect()
+        {
+            isConnecting = true;
+            progressLabel.SetActive(true);
+            controlPanel.SetActive(false);
+
+            if (PhotonNetwork.connected)
+                PhotonNetwork.JoinRandomRoom();
+            else
+                PhotonNetwork.ConnectUsingSettings(_gameVersion);
+        }
+
+        private void OnConnectedToServer()
+        {
+            Debug.Log("DemoAnimator/Launcher: OnConnectedToMaster() was called by PUN");
+        }
+
+        public override void OnConnectedToMaster()
+        {
+            Debug.Log("DemoAnimator/Launcher: OnConnectedToMaster() was called by PUN");
+            // we don't want to do anything if we are not attempting to join a room. 
+            // this case where isConnecting is false is typically when you lost or quit the game, when this level is loaded, OnConnectedToMaster will be called, in that case
+            // we don't want to do anything.
+            if (isConnecting)
+            {
+                // #Critical: The first we try to do is to join a potential existing room. If there is, good, else, we'll be called back with OnPhotonRandomJoinFailed()
+                PhotonNetwork.JoinRandomRoom();
+            }
+        }
+        public override void OnDisconnectedFromPhoton()
+        {
+            progressLabel.SetActive(false);
+            controlPanel.SetActive(true);
+
+            Debug.LogWarning("DemoAnimator/Launcher: OnDisconnectedFromPhoton() was called by PUN");
+        }
+        public override void OnPhotonRandomJoinFailed(object[] codeAndMsg)
+        {
+            Debug.Log("DemoAnimator/Launcher:OnPhotonRandomJoinFailed() was called by PUN. No random room available, so we create one.\nCalling: PhotonNetwork.CreateRoom(null, new RoomOptions() {maxPlayers = 4}, null);");
+            // #Critical: we failed to join a random room, maybe none exists or they are all full. No worries, we create a new room.
+            PhotonNetwork.CreateRoom(null, new RoomOptions() { MaxPlayers = MaxPlayersPerRoom }, null);
+        }
+
+        public void SetNickname()
+        {
+            PhotonNetwork.player.NickName = nickName.text;
+        }
+
+        public override void OnJoinedRoom()
+        {
+            // #Critical: We only load if we are the first player, else we rely on  PhotonNetwork.automaticallySyncScene to sync our instance scene.
+            if (PhotonNetwork.room.PlayerCount == 1)
+            {
+                Debug.Log("We load the 'Main Scene' ");
+
+
+                // #Critical
+                // Load the Room Level. 
+                PhotonNetwork.LoadLevel("Main Scene");
+            }
+            Debug.Log("DemoAnimator/Launcher: OnJoinedRoom() called by PUN. Now this client is in a room.");
+        }
+    }
+}
