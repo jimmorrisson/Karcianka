042b093830010d874fd40f285f3c45a5a1324185
Pc-Komputer\Pc <pyszczakjakub@gmail.com>
2018-04-16 02:21:02 +0200

    Photon synchronization on cards.

--- Karcianka/Assets/Scripts/Draggable.cs
+++ Karcianka/Assets/Scripts/Draggable.cs
@@ -12,51 +12,61 @@ public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDra
     public Transform placeHolderParent = null;
     GameObject placeHolder;
 
+    public PhotonView pView;
+
     public void OnBeginDrag(PointerEventData eventData)
-    {        
-        offset = this.transform.position - (Vector3)eventData.position;
+    {
+        if (pView.isMine)
+        {
+            offset = this.transform.position - (Vector3)eventData.position;
 
-        placeHolder = new GameObject();
-        placeHolder.transform.SetParent(this.transform.parent);
-        RectTransform rt = placeHolder.AddComponent<RectTransform>();
-        rt.sizeDelta = new Vector2(this.transform.GetComponent<RectTransform>().sizeDelta.x, this.transform.GetComponent<RectTransform>().sizeDelta.y);
+            placeHolder = new GameObject();
+            placeHolder.transform.SetParent(this.transform.parent);
+            RectTransform rt = placeHolder.AddComponent<RectTransform>();
+            rt.sizeDelta = new Vector2(this.transform.GetComponent<RectTransform>().sizeDelta.x, this.transform.GetComponent<RectTransform>().sizeDelta.y);
 
-        placeHolder.transform.SetSiblingIndex(this.transform.GetSiblingIndex());
+            placeHolder.transform.SetSiblingIndex(this.transform.GetSiblingIndex());
 
-        parentToReturnTo = this.transform.parent;
-        placeHolderParent = parentToReturnTo;
-        this.transform.SetParent(this.transform.root);
+            parentToReturnTo = this.transform.parent;
+            placeHolderParent = parentToReturnTo;
+            this.transform.SetParent(this.transform.root);
 
-        GetComponent<CanvasGroup>().blocksRaycasts = false;
+            GetComponent<CanvasGroup>().blocksRaycasts = false;
+        }
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        this.transform.position = eventData.position + offset;
+        if (pView.isMine)
+        {
+            this.transform.position = eventData.position + offset;
 
-        if (placeHolder.transform.parent != placeHolderParent)
-            placeHolder.transform.SetParent(placeHolderParent);
+            if (placeHolder.transform.parent != placeHolderParent)
+                placeHolder.transform.SetParent(placeHolderParent);
 
-        int newSiblingIndex = placeHolderParent.childCount;
-        for (int i = 0; i < placeHolderParent.childCount; i++)
-        {         
+            int newSiblingIndex = placeHolderParent.childCount;
+            for (int i = 0; i < placeHolderParent.childCount; i++)
+            {
                 if (this.transform.position.x < placeHolderParent.GetChild(i).position.x)
                 {
                     newSiblingIndex = i;
-                if (placeHolder.transform.GetSiblingIndex() < newSiblingIndex)
-                    newSiblingIndex--;
+                    if (placeHolder.transform.GetSiblingIndex() < newSiblingIndex)
+                        newSiblingIndex--;
                     break;
                 }
+            }
+            placeHolder.transform.SetSiblingIndex(newSiblingIndex);
         }
-        placeHolder.transform.SetSiblingIndex(newSiblingIndex);
     }
 
     public void OnEndDrag(PointerEventData eventData)
-    {        
-        this.transform.SetParent(parentToReturnTo);
-        this.transform.SetSiblingIndex(placeHolder.transform.GetSiblingIndex());
-        GetComponent<CanvasGroup>().blocksRaycasts = true;
-        Destroy(placeHolder);
+    {
+        if (pView.isMine)
+        {
+            this.transform.SetParent(parentToReturnTo);
+            this.transform.SetSiblingIndex(placeHolder.transform.GetSiblingIndex());
+            GetComponent<CanvasGroup>().blocksRaycasts = true;
+            Destroy(placeHolder);
+        }
     }
-
 }
